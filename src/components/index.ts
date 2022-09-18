d4f9a540613290edce70cfa80188b553155d4bef
Anders Jepsen <hi@andersjepsen.com>
2022-09-18 09:40:16 +0200

    fix: :bug: actually export header and footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,8 @@ export { default as Box } from "./Box";
 export { default as Card } from "./Card";
 export { default as Checkbox } from "./Checkbox";
 export { default as Collapisble } from "./Collapsible";
+export { default as Footer } from "./Footer";
+export { default as Header } from "./Header";
 export { default as Progress } from "./Progress";
 export { default as Slider } from "./Slider";
 export { default as Switch } from "./Switch";
